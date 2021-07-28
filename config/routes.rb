adc8f6f3dd3476c8f98759534036f31ea59badbd
EC2 Default User <ec2-user@ip-172-31-64-136.ec2.internal>
2021-07-28 14:35:10 +0000

    Unessential Commit

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'homes#top'
     get 'homes/top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
 end
