36db7b4632b4756ce46a28f99935f30084adfc5b
EC2 Default User <ec2-user@ip-172-31-64-136.ec2.internal>
2021-09-29 04:20:19 +0000

    [add]some of before_action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!
+  before_action :authenticate_customer!, except: [:top, :about]
   
   def after_sign_in_path_for(resource)
     customers_path
@@ -13,9 +13,16 @@ class ApplicationController < ActionController::Base
     root_path
   end
   
+  private
+  
+  # I think It's not working
+  # def redirect_root
+  #   redirect_to root_path unless user_signed_in?
+  # end
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
     
-private
+  private
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
